d67dd4d6b406e94c0a82c13b96724745c8060c9d
Omar-Rayan Belkhouala <112522847+nayarramo@users.noreply.github.com>
2022-12-07 19:10:41 -0500

    done

--- exercice.py
+++ exercice.py
@@ -4,20 +4,45 @@
 from collections import deque
 
 
-def get_fibonacci_number(TODO):
-	pass
+def get_fibonacci_number(nb):
+	return (0 if nb==0 else 1 if nb==1 else get_fibonacci_number(nb-1) + get_fibonacci_number(nb-2))
 
-def get_fibonacci_sequence(TODO):
-	pass
+def get_fibonacci_sequence(nb):
+	fib = [0]
+	while len(fib)<nb:
+		if len(fib)<2: fib.append(1)
+		else: fib.append(fib[-1]+fib[-2])
+	return fib
 
-def get_sorted_dict_by_decimals(TODO):
-	pass
+def get_sorted_dict_by_decimals(dct):
+	return dict(sorted(dct.items(), key= lambda x: x[1]%1))
 
 def fibonacci_numbers(length):
-	pass
+	elems = [0,1]
+	
+	for _ in range(length):
+		if elems[0] == 0:
+			yield elems.pop(0)
+			elems.append(1)
+		else:
+			elems.append(elems[0]+elems[1])
+			yield elems.pop(0)
 
-def build_recursive_sequence_generator(TODO):
-	pass
+
+def build_recursive_sequence_generator(values: list, fct, sequenceInMem=False):
+	
+	def gen(length):
+		for value in values[:length]:
+			yield value
+		last_elems = deque(values)
+		for _ in range(len(values), length):
+			elem = fct(last_elems)
+			last_elems.append(elem)
+			if not sequenceInMem:
+				last_elems.popleft()
+			yield elem
+	
+	return gen
 
 
 if __name__ == "__main__":
@@ -57,9 +82,9 @@ if __name__ == "__main__":
 		print(fi, end=" ")
 	print("\n")
 
-	lucas = build_recursive_sequence_generator(TODO)
+	lucas = build_recursive_sequence_generator([2,1], lambda lst: lst[-1]+lst[-2])
 	print(f"Lucas : {[elem for elem in lucas(10)]}")
-	perrin = build_recursive_sequence_generator(TODO)
+	perrin = build_recursive_sequence_generator([3,0,2], lambda seq: seq[-2]+seq[-3])
 	print(f"Perrin : {[elem for elem in perrin(10)]}")
-	hofstadter_q = build_recursive_sequence_generator(TODO)
+	hofstadter_q = build_recursive_sequence_generator([1,1], lambda seq: seq[-(seq[-1])] + seq[-(seq[-2])], True)
 	print(f"Hofstadter-Q : {[elem for elem in hofstadter_q(10)]}")
